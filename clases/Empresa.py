cec88b5980700ec0c86fcff0c4826313bebb3d64
EMA <emelyapraez25@gmail.com>
2023-03-17 18:08:46 -0500

    modulo empresa

--- clases/Empresa.py
+++ clases/Empresa.py
@@ -1,32 +1,11 @@
-    
-import requests
-import json
-import os
-listarEmpresasJson=json.load(open("ModulosJson/empresa/listarEmpresas.json"))
+from pydantic import BaseModel
 
 
+class leerNumeroRecibo(BaseModel):
+    idEmpresa: int = None
+    prefijo: int = None
 
-class Sesion:
 
-    def __init__(self) :
-        self.usuario=os.environ.get('usuarioItiers')
-        self.password=os.environ.get('passwordItiers')
-
-    def listarEmpresas(self,urlLogin,clave):
-        try:
-            if(clave[0]==True):
-                listarEmpresasJson['parameters']['sesion']['claveSesion']=clave[1]
-                #print(listarEmpresasJson)
-                solicitud = requests.post(urlLogin,data=json.dumps(listarEmpresasJson),headers={'Content-Type':'application/json'})
-                #print(response.status_code)
-                #print(solicitud.text)
-                #convert text to dict
-                result=json.loads(solicitud.text)
-                response=result["response"]["success"]
-                data=result["response"]["data"]
-                return [response,data]
-            else:
-                return [False,'']   
-            
-        except:
-            return "Error al listar empresas"
+class listarCCostos(BaseModel):
+    idEmpresa: int = None
+    ccosto: int = None
--- controladores/EmpresaController.py
+++ controladores/EmpresaController.py
@@ -1,17 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 from clases.Sesion import Sesion
 # libreria que se importa de configuracion.py (contiene las configuraciones del server)
 from configuracion import configuracion
 import json
 import requests
+from clases.Empresa import leerNumeroRecibo, listarCCostos
+
 listarEmpresaJson = json.load(
     open("./ModulosJson/empresa/listarEmpresas.json"))
+listarCCostosJson = json.load(
+    open("./ModulosJson/empresa/listarCCostos.json"))
+leerEmpresaJson = json.load(
+    open("./ModulosJson/empresa/leerEmpresa.json"))
+leerTalonarioJson = json.load(
+    open("./ModulosJson/empresa/leerTalonario.json"))
+leerNumeroReciboJson = json.load(
+    open("./ModulosJson/empresa/leerNumeroRecibo.json"))
+
 
 empresa_router = APIRouter()
 
 
-@empresa_router.post("/getEmpresaByColppy")
-async def getEmpresaByColppy():
+@empresa_router.post("/getEmpresasByColppy")
+async def getEmpresasByColppy():
     session = Sesion()
     login = session.iniciarSesion()
     try:
@@ -34,6 +45,160 @@ async def getEmpresaByColppy():
                     data = result['response']['data']
                     return data
                 else:
+                    return False
+
+    except Exception as e:
+        print(e)
+        return "Error al obtener datos"
+    finally:
+        if session.cerrarSesion(login) == True:
+            print("Sesion cerrada")
+
+
+
+
+
+@empresa_router.post("/getCCostosByColppy")
+async def getCCostosByColppy(parametros: listarCCostos = Body(...)):
+    session = Sesion()
+    login = session.iniciarSesion()
+    try:
+        if login[0] == True:
+            print("Sesion iniciada")
+
+            listarCCostosJson['auth']['usuario'] = configuracion['development'].USER
+            listarCCostosJson['auth']['password'] = configuracion['development'].PASSWORD
+            listarCCostosJson['parameters']['sesion']['claveSesion'] = login[1]
+            listarCCostosJson['parameters']['idEmpresa'] = str(parametros.idEmpresa)
+            listarCCostosJson['parameters']['ccosto'] = str(parametros.ccosto)
+            # Los valores posibles para 'ccosto' son "0", "1" y "2".
+
+            # print(listarCCostosJson)
+            response = requests.post(configuracion['development'].URLBASE, data=json.dumps(
+                listarCCostosJson), headers={'Content-Type': 'application/json'})
+            # print(response.status_code)
+            # print(response.text)
+            if response.status_code == 200:
+                result = json.loads(response.text)
+                success = result["response"]["success"]
+                if success == True:
+                    data = result['response']['codigos']
+                    return data
+                else:
+                    return False
+
+    except Exception as e:
+        print(e)
+        return "Error al obtener datos"
+    finally:
+        if session.cerrarSesion(login) == True:
+            print("Sesion cerrada")
+
+
+
+
+@empresa_router.post("/getEmpresaByColppy/{idEmpresa}")
+async def getEmpresaByColppy(idEmpresa: int):
+    session = Sesion()
+    login = session.iniciarSesion()
+    try:
+        if login[0] == True:
+            print("Sesion iniciada")
+
+            leerEmpresaJson['auth']['usuario'] = configuracion['development'].USER
+            leerEmpresaJson['auth']['password'] = configuracion['development'].PASSWORD
+            leerEmpresaJson['parameters']['sesion']['claveSesion'] = login[1]
+            leerEmpresaJson['parameters']['idEmpresa'] = str(idEmpresa)
+
+            # print(leerEmpresaJson)
+            response = requests.post(configuracion['development'].URLBASE, data=json.dumps(
+                leerEmpresaJson), headers={'Content-Type': 'application/json'})
+            # print(response.status_code)
+            # print(response.text)
+            if response.status_code == 200:
+                result = json.loads(response.text)
+                success = result["response"]["success"]
+                if success == True:
+                    data = result['response']['data']
+                    return data
+                else:
+                    return False
+
+    except Exception as e:
+        print(e)
+        return "Error al obtener datos"
+    finally:
+        if session.cerrarSesion(login) == True:
+            print("Sesion cerrada")
+
+
+# talonarioDefault y descTipoComprobante son otros parametros OPCIONALES
+@empresa_router.post("/getTalonarioColppy/{idEmpresa}")
+async def getTalonarioColppy(idEmpresa: int):
+    session = Sesion()
+    login = session.iniciarSesion()
+    try:
+        if login[0] == True:
+            print("Sesion iniciada")
+
+            leerTalonarioJson['auth']['usuario'] = configuracion['development'].USER
+            leerTalonarioJson['auth']['password'] = configuracion['development'].PASSWORD
+            leerTalonarioJson['parameters']['sesion']['claveSesion'] = login[1]
+            leerTalonarioJson['parameters']['idEmpresa'] = str(idEmpresa)
+
+            # print(leerTalonarioJson)
+            response = requests.post(configuracion['development'].URLBASE, data=json.dumps(
+                leerTalonarioJson), headers={'Content-Type': 'application/json'})
+            # print(response.status_code)
+            # print(response.text)
+            if response.status_code == 200:
+                result = json.loads(response.text)
+                success = result["response"]["success"]
+                if success == True:
+                    data = result['response']['data']
+                    return data
+                else:
+                    return False
+
+    except Exception as e:
+        print(e)
+        return "Error al obtener datos"
+    finally:
+        if session.cerrarSesion(login) == True:
+            print("Sesion cerrada")
+
+
+
+
+
+@empresa_router.post("/getNumeroReciboColppy")
+async def getNumeroReciboColppy(parametros: leerNumeroRecibo = Body(...)):
+    session = Sesion()
+    login = session.iniciarSesion()
+    try:
+        if login[0] == True:
+            print("Sesion iniciada")
+
+            leerNumeroReciboJson['auth']['usuario'] = configuracion['development'].USER
+            leerNumeroReciboJson['auth']['password'] = configuracion['development'].PASSWORD
+            leerNumeroReciboJson['parameters']['sesion']['claveSesion'] = login[1]
+            leerNumeroReciboJson['parameters']['idEmpresa'] = str(parametros.idEmpresa)
+            leerNumeroReciboJson['parameters']['prefijo'] = str(parametros.prefijo)
+            # 'prefijo' = Primeros cuatro dígitos de un número de factura.
+
+            # print(leerNumeroReciboJson)
+            response = requests.post(configuracion['development'].URLBASE, data=json.dumps(
+                leerNumeroReciboJson), headers={'Content-Type': 'application/json'})
+            # print(response.status_code)
+            #print(response.text)
+            if response.status_code == 200:
+                result = json.loads(response.text)
+                success = result["response"]["success"]
+                if success == True:
+                    data = result['response']['data']
+                    return data
+                else:
+                    print(result["response"]["message"])
                     return False
 
     except Exception as e:
